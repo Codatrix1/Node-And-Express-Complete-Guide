6cce9d7daf558441a342cee988ae8e731adc1e69
Codatrix <ankur.codatrix@gmail.com>
2022-07-06 10:19:09 +0530

    HTTP - Request Object

--- 02-Express/app.js
+++ 02-Express/app.js
@@ -1,15 +1,41 @@
-// imports
+//----------
+// IMPORTS
+//----------
 const colors = require("colors");
 const http = require("http");
 
-// create server
+//---------------
+// CREATE SERVER
+//---------------
 const server = http.createServer((req, res) => {
-  res.writeHead(200, { "content-type": "text/html" });
-  res.write("<h1>Home Page</h1>");
-  res.end();
+  //   console.log(req.method);
+  //   console.log(req.url);
+
+  const url = req.url;
+
+  // Home
+  if (url === "/") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>Home Page</h1>");
+    res.end();
+
+    // About
+  } else if (url === "/about") {
+    res.writeHead(200, { "content-type": "text/html" });
+    res.write("<h1>About Page</h1>");
+    res.end();
+
+    // Not Found
+  } else {
+    res.writeHead(404, { "content-type": "text/html" });
+    res.write("<h1>Oops! Page Not Found</h1>");
+    res.end();
+  }
 });
 
-// listen to server
+//-------------------
+// LISTEN TO SERVER
+//-------------------
 server.listen(5000, () => {
   console.log(`Application running on port 5000...`.yellow.bold);
 });
