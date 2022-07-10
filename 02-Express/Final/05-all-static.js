9b0e52c801951a11fbbf2deb897f69d739f1da54
Codatrix <ankur.codatrix@gmail.com>
2022-07-10 08:39:01 +0530

    Express - App Example & Express - All Static

---
+++ 02-Express/Final/05-all-static.js
@@ -0,0 +1,30 @@
+const express = require("express");
+const app = express();
+
+const colors = require("colors");
+const path = require("path");
+
+//------------------------------------
+// Serve Static Assets and Middleware
+//------------------------------------
+app.use(express.static("./public"));
+
+//-------------------
+// Express Methods
+//-------------------
+// app.get("/", (req, res) => {
+//   res.status(200).sendFile(path.resolve(__dirname, "./navbar-app/index.html"));
+//   adding to static Assets
+//   SSR
+// });
+
+app.get("*", (req, res) => {
+  res.status(404).send("Resource not found");
+});
+
+//---------------
+// Start SERVER
+//---------------
+app.listen(5000, () => {
+  console.log(`Server running on port 5000...`.yellow.bold);
+});
