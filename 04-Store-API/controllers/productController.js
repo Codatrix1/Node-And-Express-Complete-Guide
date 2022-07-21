cd56b5a7d82cfd52de2831135b55042627bcefeb
Codatrix <ankur.codatrix@gmail.com>
2022-07-21 12:30:15 +0530

    Sort - getAllProducts Implementation

--- 04-Store-API/controllers/productController.js
+++ 04-Store-API/controllers/productController.js
@@ -3,16 +3,18 @@ const Product = require("../models/productModel");
 const getAllProductsStatic = async (req, res) => {
   // const products = await Product.find({ featured: true });
   // const products = await Product.find({ name: "albany " });
-  const search = "wo";
-  const products = await Product.find({
-    name: { $regex: search, $options: "i" },
-  });
+
+  // const search = "wo";
+  // const products = await Product.find({
+  //   name: { $regex: search, $options: "i" },
+  // });
+  const products = await Product.find({}).sort("-name price");
   res.status(200).json({ nbHits: products.length, products });
 };
 
 const getAllProducts = async (req, res) => {
   // Destructure the properties off the query string
-  const { featured, company, name } = req.query;
+  const { featured, company, name, sort } = req.query;
 
   // Define an Empty Query Object
   const queryObject = {};
@@ -29,9 +31,21 @@ const getAllProducts = async (req, res) => {
     queryObject.name = { $regex: name, $options: "i" };
   }
 
-  console.log(queryObject);
+  // console.log(queryObject);
 
-  const products = await Product.find(queryObject);
+  let result = Product.find(queryObject);
+
+  if (sort) {
+    // console.log(sort);
+    const sortBy = sort.split(",").join(" ");
+    // console.log(sortBy);
+    result = result.sort(sortBy);
+  } else {
+    // default sort
+    result = result.sort("createdAt _id");
+  }
+
+  const products = await result;
   res.status(200).json({ nbHits: products.length, products });
 };
 
