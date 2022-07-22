f5abcd0f5c6b95e660492190b2df44a9896fa118
Codatrix <ankur.codatrix@gmail.com>
2022-07-22 10:05:23 +0530

    Validate username and password w/ CustomAPIError Class and errorHandlerMiddleware

--- 05-JWT-Explained/controllers/mainController.js
+++ 05-JWT-Explained/controllers/mainController.js
@@ -1,4 +1,22 @@
+// check username,password in POST(login) request
+// if exists create JWT
+// send to frontend
+
+// setup authentication so the only request with JWT can access the dashboard
+
+const CustomAPIError = require("../errors/custom-error");
+
 const login = async (req, res) => {
+  const { username, password } = req.body;
+
+  // Mongoose validation
+  // Joi package
+  // manual check in the controller
+
+  if (!username || !password) {
+    throw new CustomAPIError("Please provide username and password", 400);
+  }
+
   res.send("Fake Login/Register/SignUp");
 };
 
