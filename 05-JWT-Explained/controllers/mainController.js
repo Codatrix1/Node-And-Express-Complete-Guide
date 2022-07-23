f5b015b0875b53c35931974d7daf70969f5d2cd0
Codatrix <ankur.codatrix@gmail.com>
2022-07-23 09:47:38 +0530

    Send Bearer Token - Check Auth Header - Verify Token

--- 05-JWT-Explained/controllers/mainController.js
+++ 05-JWT-Explained/controllers/mainController.js
@@ -33,12 +33,27 @@ const login = async (req, res) => {
 };
 
 const dashboard = async (req, res) => {
-  const luckyNumber = Math.floor(Math.random() * 100);
+  const authHeader = req.headers.authorization;
 
-  res.status(200).json({
-    msg: `Hello Jane Doe`,
-    secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
-  });
+  if (!authHeader || !authHeader.startsWith("Bearer")) {
+    throw new CustomAPIError("No token provided", 401);
+  }
+
+  const token = authHeader.split(" ")[1];
+
+  try {
+    const decoded = jwt.verify(token, process.env.JWT_SECRET);
+    // console.log(decoded);
+
+    const luckyNumber = Math.floor(Math.random() * 100);
+
+    res.status(200).json({
+      msg: `Hello ${decoded.username}`,
+      secret: `Here is your authorized data, your lucky number is ${luckyNumber}`,
+    });
+  } catch (error) {
+    throw new CustomAPIError("Not authorized to access this route", 401);
+  }
 };
 
 module.exports = { login, dashboard };
